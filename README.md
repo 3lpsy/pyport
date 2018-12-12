0c29e3606052248d00550c8e0d7ee55b1d960627
Jim <3lpsy@users.noreply.github.com>
2018-12-12 06:08:01 -0500

    complex port formats and README

---
+++ README.md
@@ -0,0 +1,34 @@
+# Pyport
+
+A simple multi threaded port scanner written in python3 with no dependencies.
+
+```
+
+usage: pyport.py [-h] -t TARGET [-m MIN_PORT] [-M MAX_PORT] [-T THREADS]
+                 [--timeout TIMEOUT] [--verbose] [--no-random]
+                 [-P {tcp,udp,http,https}] [-p PORT [PORT ...]]
+
+optional arguments:
+  -h, --help            show this help message and exit
+  -t TARGET, --target TARGET
+                        desired target (ip)
+  -m MIN_PORT, --min-port MIN_PORT
+                        min port (range)
+  -M MAX_PORT, --max-port MAX_PORT
+                        max port (range)
+  -T THREADS, --threads THREADS
+                        do you want to go fast?
+  --timeout TIMEOUT     set timeout
+  --verbose, -v         verbosity (-v, -vv, -vvv, -vvvv)
+  --no-random           don't randomize port order
+  -P {tcp,udp,http,https}, --proto {tcp,udp,http,https}
+                        protocol
+  -p PORT [PORT ...], --port PORT [PORT ...]
+                        ports (-p 1 2 3, -p 1-5, or -p 1,2,3)
+
+```
+
+TODO:
+
+[ ] Need to catch the requests exception for HTTP/S to avoid displaying too much information if verbosity is less than 3
+[ ] unique ports if passing in complex port formats
--- pyport.py
+++ pyport.py
@@ -120,19 +120,44 @@ class ScanManager(object):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('-t', '--target', type=str, required=True)
-    parser.add_argument('-m', '--min-port', type=int, default=1)
-    parser.add_argument('-M', '--max-port', type=int, default=65535)
-    parser.add_argument('-T', '--threads', type=int, default=10)
-    parser.add_argument('--timeout', type=int, default=10)
-    parser.add_argument('--verbose', '-v', action='count',default=0)
-    parser.add_argument('--no-random', action="store_true", default=False)
-    parser.add_argument('-p', '--proto', type=str, default="tcp", choices=['tcp', 'udp', 'http', 'https'])
+    parser.add_argument('-t', '--target', type=str, required=True, help="desired target (ip)")
+    parser.add_argument('-m', '--min-port', type=int, default=1, help="min port (range)")
+    parser.add_argument('-M', '--max-port', type=int, default=65535, help="max port (range)")
+    parser.add_argument('-T', '--threads', type=int, default=10, help="do you want to go fast?")
+    parser.add_argument('--timeout', type=int, default=10, help="set timeout")
+    parser.add_argument('--verbose', '-v', action='count',default=0, help="verbosity (-v, -vv, -vvv, -vvvv)")
+    parser.add_argument('--no-random', action="store_true", default=False, help="don't randomize port order")
+    parser.add_argument('-P', '--proto', type=str, default="tcp", choices=['tcp', 'udp', 'http', 'https'], help="protocol")
+    parser.add_argument('-p', '--port',nargs='+', type=str, default=None, required=False, help="ports (-p 1 2 3, -p 1-5, or -p 1,2,3)")
 
     args = parser.parse_args()
 
     target = args.target
-    ports = list(range(args.min_port, args.max_port))
+
+    if not args.port:
+        ports = list(range(args.min_port, args.max_port))
+    else:
+        ports = []
+        specified_ports = list(args.port)
+        for p in specified_ports:
+            more_ports = []
+            if '-' in p:
+                port_split = p.split('-')
+                print(port_split)
+                if len(port_split) > 2:
+                    raise Exception("Port format incorrect. {} in ".format(p, str(specified_ports)))
+                _min = int(port_split[0])
+                _max = int(port_split[1]) + 1
+                more_ports = list(range(_min, _max))
+            elif ',' in p:
+                if p.endswith(','):
+                    raise Exception("Port format incorrect. Canot end in comma. {} in ".format(p, str(specified_ports)))
+                for _p in p.split(','):
+                    more_ports.append(int(_p))
+            else:
+                _p = int(p)
+                more_ports.append(_p)
+            ports = ports + more_ports
 
     if not args.no_random:
         shuffle(ports)
@@ -150,7 +175,10 @@ if __name__ == '__main__':
 
     print("[*] pyport starting")
     print("[*] target", target)
-    print("[*] ports", len(ports), 'total')
+    if len(ports) < 11:
+        print("[*] ports", ports)
+    else:
+        print("[*] ports", len(ports), 'total')
     print("[*] protocol", proto)
     print("[*] threads", str(threads))
     print("[*] timeout", timeout)
